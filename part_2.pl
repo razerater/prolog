849e168c30dee56654547a7df1644d8b528d21ee
Alston <Achau9>
2019-11-25 16:56:29 -0500

    Parsing input to list of string

---
+++ part_2.pl
@@ -0,0 +1,40 @@
+
+main(W) :-
+
+  %Parsing input which is a list containg a single string Ex: ["Input goes here"]
+  %Converting it to a list of strings. Ex:"["Input", "goes", "here"]"
+  atomic_list_concat(W,' ',L),
+  atom_string(L,Sent),
+  split_string(Sent," "," ",List),
+
+  %If and Else statement. Prints out list if it matches sentence parse else invalid.
+  (sentence(List,[]) ->
+    print(List),nl
+    ; print("This is an invalid string"),nl
+    ).
+  %Doesn't print anything if input is not part of sentence
+sentence --> verb,article,noun.
+article --> ["a"].
+noun -->["set"].
+verb -->["find"].
+  % print(L).
+  % format('~s\n', W).
+
+  % main(S):-
+  %   2 is round(sqrt(4)),
+  %   write(one).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Commmands for running programming
+
+%%%Using text files as input
+% in=$(cat in1.txt)
+% swipl -q -f test.pl -t main "$in" > out1.txt
+% python autograder.py 1
+
+%%%Using string as input
+% swipl -q -f test.pl -t main "Example Input String"
+
+% ?- split_string("This is an example"," "," ",L)
+% |    .
+% L = ["This", "is", "an", "example"].
