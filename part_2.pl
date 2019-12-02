e0bcd79f435be9d36f60b0428b7566fae16be8bb
Alston <Achau9>
2019-12-01 23:57:50 -0500

    Numbers extracted

--- part_2.pl
+++ part_2.pl
@@ -7,8 +7,17 @@ main(W) :-
   split_string(Sent," "," ",List),
 
   %If and Else statement. Prints out list if it matches sentence parse else invalid.
-  (sentence(List,[]) ->
-    print(List),nl
+  (phrase(sentence,List) ->
+    phrase(number,ListNum),
+    atomic_list_concat(ListNum,'',Lis1),
+    atom_string(Lis1,Char1),
+    number_codes(Num,Char1),
+    phrase(sum,ListSum),
+    atomic_list_concat(ListSum,'',Lis2),
+    atom_string(Lis2,Char2),
+    number_codes(Sum,Char2),
+    format("Number:~d Sum:~d\n",[Num,Sum])
+    % print(Sum),nl
     ; format("Invalid String"), nl
     ).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -65,14 +74,17 @@ mulTo([H|T], X) :-
   Y is X/H,
   sumTo(T, Y).
 
+
 naturalNumber(0).
 naturalNumber(X):-
   naturalNumber(Y),
   X is Y+1.
 
 
+
 %Example input: Find a set of 3 odd integers that sum to 15
 %Output:3,5,7
+%Input: Find a set of 2 even integers that sum to 6
 
 %We want ot break up the sentence "Find a set of 3 odd integers that sum to 15" as:
 % sentence("Find a set of") I("3 odd integers") -> I(O("3 odd")) pronoun("that") Op("sum")  preposition("to") number("15")
@@ -82,7 +94,7 @@ naturalNumber(X):-
 % integer_phrase_even --> number, "even", noun, .
 
 
-sentence --> sent_phrase,integer_phrase,pronoun,op,preposition,number.
+sentence --> sent_phrase,integer_phrase,pronoun,op,preposition,sum.
 sent_phrase --> verb,article,noun, preposition.
 integer_phrase --> number, int_property, noun.
 integer_phrase --> number, int_property,conjuction,number, int_property,noun.
@@ -94,5 +106,6 @@ conjuction  --> ["and"].
 article --> ["a"].
 noun --> ["set"] | ["integers"].
 verb --> ["Find"].
-number --> ["3"] | ["15"] | ["120"].
+number --> ["3"].
+sum --> ["15"].
 % naturalNumber(X) -->
